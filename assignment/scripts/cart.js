3ad7c2734aa7f2e2d76b0d01e3f6ef422f840362
Tony LaForgia <alforgia428@gmail.com>
2022-01-09 15:41:32 -0600

    Completed WK 4 part 3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,8 +2,27 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
 
 function addItem( item ){
-  const basket = [];
+  console.log(`Basket is ${basket}`);
+  console.log(`Adding ${item} (expect true)`);
+  basket.push(item);
+  console.log(`Basket is now ${basket}`);
 
 }
+addItem('jar');
+addItem('cup');
+addItem('pen');
+function listItems(){
+  for (let i = 0; i < basket.length; i++) {
+    console.log(basket[i]);
+  }
+}
+listItems()
+
+function empty(){
+  basket = [];
+}
+empty();
+console.log(`Basket is ${basket.length === 0 ? 'empty' : basket}`);
